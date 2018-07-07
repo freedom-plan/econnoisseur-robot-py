cfe095bfabf9ccf18a1057083e92fdd16b27cc42
norman <norman@5dgravity.com>
2018-07-07 21:03:43 +0800

    add common service

---
+++ service/common_service.py
@@ -0,0 +1,84 @@
+# coding=utf-8
+
+from dto.common import *
+
+
+class MarketService(object):
+    def get_ticker(self, pair: str) -> Ticker:
+        """ 获取指定币种的ticker
+
+        :rtype: Ticker 详情
+        :param pair: 交易对
+        """
+        raise RuntimeError('Not implement yet!')
+
+    def get_depth(self, pair: str) -> list:
+        """ 获取市场深度
+
+        :param pair: coin pair symbol
+        :rtype: list Depth list
+        """
+        raise RuntimeError('Not implement yet!')
+
+
+class AccountService(object):
+    def __init__(self, account):
+        self.name = account.name
+        self._secretKey = account.secretKey
+        self._apiKey = account.apiKey
+
+    def _refresh(self):
+        raise RuntimeError('Not implement yet!')
+
+    def get_balance_by_coins(self, coins) -> dict:
+        """ 获取指定币种余额
+
+        :rtype: dict of coin balance, key-> coin, value-> Balance
+        :param coins: coin list
+        """
+        raise RuntimeError('Not implement yet!')
+
+
+class OrderService(object):
+    def __init__(self, account):
+        self.name = account.name
+        self._secretKey = account.secretKey
+        self._apiKey = account.apiKey
+
+    def buy(self, order: Order) -> str:
+        """ 发送买单
+
+        :rtype: str 订单号
+        :param order: 订单详情
+        """
+        raise RuntimeError('Not implement yet!')
+
+    def sell(self, order: Order) -> str:
+        """ 发送卖单
+
+        :rtype: str 订单号
+        :param order: 订单详情
+        """
+        raise RuntimeError('Not implement yet!')
+
+    def cancel(self, order_id: str, **kwargs) -> bool:
+        """ 取消指定订单
+
+        :type kwargs: 特殊参数
+        :param order_id: 订单号
+        """
+        raise RuntimeError('Not implement yet!')
+
+    def cancel_all(self):
+        """ 取消所有未成交订单
+
+        """
+        raise RuntimeError('Not implement yet!')
+
+    def status(self, order_id: str, **kwargs) -> OrderStatus:
+        """ 检查指定订单的状态
+        :rtype: object
+        :param order_id: 订单号
+
+        """
+        raise RuntimeError('Not implement yet!')
